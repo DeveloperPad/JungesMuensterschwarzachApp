8f8a73326824b10a4f0d3a0bc1ebbf40e8ac487e
Lucas Kinne <lucas@luckev.info>
2022-06-07 19:13:22 +0200

    Add path to 404 page

--- app/src/js/components/pages/PageNotFound.tsx
+++ app/src/js/components/pages/PageNotFound.tsx
@@ -3,10 +3,12 @@ import * as React from "react";
 import { Card, CardContent, CardHeader, WithTheme, withTheme } from "@material-ui/core";
 
 import Background from "../utilities/Background";
+import { useLocation } from "react-router";
 
 type IPageNotFoundProps = WithTheme;
 
 const PageNotFound = (props: IPageNotFoundProps) => {
+    const location = useLocation();
     const { theme } = props;
 
     return (
@@ -14,7 +16,7 @@ const PageNotFound = (props: IPageNotFoundProps) => {
             <Card>
                 <CardHeader title="HTTP-Status-Code: 404" />
                 <CardContent>
-                    Page not found.
+                    Page not found: { location.pathname }
                 </CardContent>
             </Card>
         </Background>
